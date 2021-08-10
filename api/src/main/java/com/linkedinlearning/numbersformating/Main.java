e3bf0d62b93864d5bbb7fa9a608242b3ecc809a1
Cesar Augusto Rayo Zapata <cesar-rayo@hotmail.es>
2021-08-10 16:26:31 -0500

    playing with formats

---
+++ api/src/main/java/com/linkedinlearning/numbersformating/Main.java
@@ -0,0 +1,40 @@
+package com.linkedinlearning.numbersformating;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args){
+        //double doubleValue = 1234567.89;
+        double doubleValue = 1_234_567.89;
+
+        NumberFormat numberFormat = NumberFormat.getNumberInstance();
+        System.out.println(numberFormat.format(doubleValue));
+
+        NumberFormat intFormat = NumberFormat.getIntegerInstance();
+        System.out.println(intFormat.format(doubleValue));
+
+        numberFormat.setGroupingUsed(false);
+        System.out.println(numberFormat.format(doubleValue));
+
+        Locale locale = new Locale("de", "DE");
+        NumberFormat localFormat = NumberFormat.getNumberInstance(locale);
+        System.out.println(localFormat.format(doubleValue));
+
+        Locale locale2 = new Locale("en", "US");
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale2);
+        System.out.println(currencyFormat.format(doubleValue));
+
+        DecimalFormat df = new DecimalFormat("$000.00");
+        DecimalFormat df2 = new DecimalFormat("$000.00#");
+        //$001.00
+        System.out.println(df.format(1));
+
+        //$005.89
+        System.out.println(df.format(5.891));
+
+        //$005.891
+        System.out.println(df2.format(5.891));
+    }
+}
